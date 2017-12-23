9c97dd12e472deacb7af2576dfb6e6042431ba6d
emilyhosoya <emilyhosoya@gmail.com>
2017-12-23 16:23:59 -0700

    Fixed app.js db errors

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
   $(".datepicker").pickadate({
     selectMonths: true, // Creates a dropdown to control month
@@ -9,46 +8,68 @@ $(document).ready(function() {
     closeOnSelect: false // Close upon selecting a date,
   });
   $("select").material_select();
-  
-  // Initialize Firebase
-  var firstName = "";
-  var lastName = "";
-  var email = "";
-  
-  var dob = 0;
-  var upload = "";
-  var submit = "";
+
+  var config = {
+    // Kairos Keys
+    app_id: "299078c0",
+    app_key: "0004235442d8fe37c6a315b2de0a40e8",
+    // Firebase Keys
+    apiKey: "AIzaSyDUnxL1x14WGo0ZFV2okfnh6ekHWVsL6Hs",
+    authDomain: "facepal-a9ecc.firebaseapp.com",
+    databaseURL: "https://facepal-a9ecc.firebaseio.com",
+    projectId: "facepal-a9ecc",
+    storageBucket: "facepal-a9ecc.appspot.com",
+    messagingSenderId: "603932838503"
+  };
+
+  let firstName = "";
+  let lastName = "";
+  let email = "";
+  let dob = 0;
+  let img = "";
 
   firebase.initializeApp(config);
-  var ref= firebase.database().ref();
+  let ref = firebase.database().ref();
   // Initialize Materialize elements
   $("#submit").on("click", function(event) {
     event.preventDefault();
-    firstName = $("#firstName").val().trim();
-    email = $("#email").val().trim();
-    lastName = $("#lastName").val().trim();
-    datepicker = $("#dob").val().trim();
-    upload = $("#upload").val().trim();
-    submit = $("#submit").val().trim();
+    firstName = $("#firstName")
+      .val()
+      .trim();
+    email = $("#email")
+      .val()
+      .trim();
+    lastName = $("#lastName")
+      .val()
+      .trim();
+    dob = $("#dob")
+      .val()
+      .trim();
+    img = $("#img")
+      .val()
+      .trim();
 
     ref.push({
       firstName: firstName,
       lastName: lastName,
       email: email,
       dob: dob,
-      upload : upload,
-      submit : submit,
+      img: img
     });
   });
 
-  ref.on("child_added", function(snapshot) {
-    // Log everything that's coming out of snapshot
-    console.log(snapshot.val());
-    console.log(snapshot.val().firstName);
-    console.log(snapshot.val().lastName);
-    console.log(snapshot.val().dob);
-    console.log(snapshot.val().upload);
-  }, function(errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-  });
-})
+  ref.on(
+    "child_added",
+    function(snapshot) {
+      // Log everything that's coming out of snapshot
+      console.log(snapshot.val());
+      console.log(snapshot.val().firstName);
+      console.log(snapshot.val().lastName);
+      console.log(snapshot.val().dob);
+      console.log(snapshot.val().upload);
+    },
+    function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    }
+  );
+});
