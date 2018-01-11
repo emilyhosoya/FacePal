bbee642cc112f0cbe0a1eb6a6ed6e0272fe9567a
emilyhosoya <emilyhosoya@gmail.com>
2018-01-10 20:29:49 -0700

    Made login modal prettier

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -9,16 +9,14 @@ var config = {
 firebase.initializeApp(config);
 
 // Get the modal
-var modal = document.getElementById('id01');
+var modal = document.getElementById("id01");
 
-function hideLogin()
-{
-  $('#id01').modal('close');
+function hideLogin() {
+  $("#id01").modal("close");
 }
 
-function showLogin()
-{
-  $('body').prepend(`<div id="id01" class="modal">
+function showLogin() {
+  $("body").prepend(`<div id="id01" class="modal">
   
   <form class="modal-content animate" action="/action_page.php">
     <div class="imgcontainer">
@@ -27,24 +25,19 @@ function showLogin()
     </div>
 
     <div class="container">
-      <label><b>email</b></label>
-      <input type="text" placeholder="email" name="uname" required>
+      <label><b>Email:</b></label>
+      <input type="text" placeholder="Your Email" name="uname" required>
 
-      <label><b>Password</b></label>
+      <label><b>Password:</b></label>
       <input type="password" placeholder="Enter Password" name="password" required>
         
-      <button type="submit">Login</button>
-      <input type="checkbox" checked="checked"> Remember me
-    </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="hideLogin()" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
+      <button class="btn" type="submit">Login</button>
+      <button type="button" onclick="hideLogin()" class="btn cancelbtn">Cancel</button>
     </div>
   </form>
 </div>`);
-$('#id01').modal();
-$('#id01').modal('open');
+  $("#id01").modal();
+  $("#id01").modal("open");
 
   // $.ajax({url: './test.html', method: 'get'}).done(response =>
   // {
@@ -57,71 +50,82 @@ $('#id01').modal('open');
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = (function(event) {
   if (event.target == modal) {
-      modal.style.display = "none";
+    modal.style.display = "none";
   }
-}
-
-    (function() {
-      var app = document.querySelector('#app');
-      app.signIn = function() {
-        var email = app.email;
-        var password = app.password;
-        if (!email || !password) {
-          return console.log('email and password required');
-        }
-        // Sign in user
-        firebase.auth().signInWithEmailAndPassword(email, password)
-          .catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log('signIn error', error);
-            // ...
-          });
-      };
-      app.register = function() {
-        var email = app.email;
-        var password = app.password;
-        if (!email || !password) {
-          return console.log('email and password required');
-        }
-        // Register user
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-          .catch(function(error) {
-            console.log('register error', error);
-            if (error.code === 'auth/email-already-in-use') {
-              var credential = firebase.auth.EmailAuthProvider.credential(email, password);
-              app.signInWithGoogle()
-                .then(function() {
-                  firebase.auth().currentUser.link(credential)
-                    .then(function(user) {
-                      console.log("Account linking success", user);
-                    }, function(error) {
-                      console.log("Account linking error", error);
-                    });
-                });
-            }
-          });
-      };
-      app.signInWithGoogle = function() {
-        // Sign in with Google
-        var provider = new firebase.auth.GoogleAuthProvider();
-        provider.addScope('profile');
-        provider.addScope('email');
-        return firebase.auth().signInWithPopup(provider)
-          .catch(function(error) {
-            console.log('Google sign in error', error);
-          });
-      };
-      app.signOut = function() {
-        // Sign out
-        firebase.auth().signOut();
-      };
-      // Listen to auth state changes
-      firebase.auth().onAuthStateChanged(function(user) {
-        app.user = user;
-        console.log('user', user);
+})(function() {
+  var app = document.querySelector("#app");
+  app.signIn = function() {
+    var email = app.email;
+    var password = app.password;
+    if (!email || !password) {
+      return console.log("email and password required");
+    }
+    // Sign in user
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("signIn error", error);
+        // ...
       });
-    })();
+  };
+  app.register = function() {
+    var email = app.email;
+    var password = app.password;
+    if (!email || !password) {
+      return console.log("email and password required");
+    }
+    // Register user
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        console.log("register error", error);
+        if (error.code === "auth/email-already-in-use") {
+          var credential = firebase.auth.EmailAuthProvider.credential(
+            email,
+            password
+          );
+          app.signInWithGoogle().then(function() {
+            firebase
+              .auth()
+              .currentUser.link(credential)
+              .then(
+                function(user) {
+                  console.log("Account linking success", user);
+                },
+                function(error) {
+                  console.log("Account linking error", error);
+                }
+              );
+          });
+        }
+      });
+  };
+  app.signInWithGoogle = function() {
+    // Sign in with Google
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope("profile");
+    provider.addScope("email");
+    return firebase
+      .auth()
+      .signInWithPopup(provider)
+      .catch(function(error) {
+        console.log("Google sign in error", error);
+      });
+  };
+  app.signOut = function() {
+    // Sign out
+    firebase.auth().signOut();
+  };
+  // Listen to auth state changes
+  firebase.auth().onAuthStateChanged(function(user) {
+    app.user = user;
+    console.log("user", user);
+  });
+})();
