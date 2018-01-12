d003f47dc6fa43c84958438c5286d263c461ccb2
garviswbs <30781787+garviswbs@users.noreply.github.com>
2018-01-12 14:11:11 -0700

    move webcam.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,81 +40,6 @@ $(document).ready(function() {
 
   let verifyImage = "";
 
-var webcamModule = function() {
-  var streaming = false;
-  var video = null;
-
-  // image return
-
-  // console.log("TEST" +imageData);
-///////////////////////////////////////////////////
-///DEBUG
-  (function() {
-    video = document.getElementById("webcamVideo");
-    navigator.mediaDevices
-      .getUserMedia({ audio: false, video: true })
-      .then(function(stream) {
-        if (navigator.mozGetUserMedia) {
-          video.mozSrcObject = stream;
-        } else {
-          var vendorURL = window.URL || window.webkitURL;
-          video.src = vendorURL.createObjectURL(stream);
-        }
-        video.play();
-        localStream = stream.getTracks()[0];
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-    video.addEventListener(
-      "canplay",
-      function(ev) {
-        if (!streaming) {
-          video.setAttribute("width", "600");
-          video.setAttribute("height", "450");
-          streaming = true;
-        }
-        var captureInterval = 5000;
-        var countdown = captureInterval / 1000;
-        var counterFunction = setInterval(function() {
-          $("#showCounter").html(countdown);
-
-          //Take the picture
-          if (countdown <= 0) {
-            takepicture(video);
-            clearInterval(counterFunction);
-            localStream.stop();
-          }
-          countdown--;
-        }, 1000);
-      },
-      false
-    );
-  })();
-
-  console.log("verify image: " + verifyImage);
-};
-
-var takepicture = function(video) {
-  $("#showCounter").html("Retrieving data...");
-  var canvas = document.createElement("CANVAS");
-  var context = canvas.getContext("2d");
-  canvas.width = "600";
-  canvas.height = "450";
-  // draw video image onto canvas, get data
-  context.drawImage(video, 0, 0);
-  var imageData = canvas.toDataURL("image/png");
-  $("#showCounter").html("See image data in console.");
-  $(video).hide();
-  console.log(imageData); ///return
-
-  //return imageData;
-  verifyImage = imageData.split(",")[1];
-  console.log("after split: " + verifyImage);
-  //Copy image data
-};
-
-//////////////////////////////////////////////////////////////
 
 
   firebase.initializeApp(config);
@@ -198,6 +123,79 @@ var takepicture = function(video) {
     }
   });
 
+  ///////////////////////////////////////////////////
+///DEBUG
+
+var webcamModule = function() {
+  var streaming = false;
+  var video = null;
+  // image return
+  (function() {
+    video = document.getElementById("webcamVideo");
+    navigator.mediaDevices
+      .getUserMedia({ audio: false, video: true })
+      .then(function(stream) {
+        if (navigator.mozGetUserMedia) {
+          video.mozSrcObject = stream;
+        } else {
+          var vendorURL = window.URL || window.webkitURL;
+          video.src = vendorURL.createObjectURL(stream);
+        }
+        video.play();
+        localStream = stream.getTracks()[0];
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+    video.addEventListener(
+      "canplay",
+      function(ev) {
+        if (!streaming) {
+          video.setAttribute("width", "600");
+          video.setAttribute("height", "450");
+          streaming = true;
+        }
+        var captureInterval = 5000;
+        var countdown = captureInterval / 1000;
+        var counterFunction = setInterval(function() {
+          $("#showCounter").html(countdown);
+          //Take the picture
+          if (countdown <= 0) {
+            takepicture(video);
+            clearInterval(counterFunction);
+            localStream.stop();
+          }
+          countdown--;
+        }, 1000);
+      },
+      false
+    );
+  })();
+
+console.log("verify image: " + verifyImage);
+};
+
+var takepicture = function(video) {
+  $("#showCounter").html("Retrieving data...");
+  var canvas = document.createElement("CANVAS");
+  var context = canvas.getContext("2d");
+  canvas.width = "600";
+  canvas.height = "450";
+  // draw video image onto canvas, get data
+  context.drawImage(video, 0, 0);
+  var imageData = canvas.toDataURL("image/png");
+  $("#showCounter").html("See image data in console.");
+  $(video).hide();
+  console.log(imageData); ///return
+
+  //return imageData;
+  verifyImage = imageData.split(",")[1];
+  console.log("after split: " + verifyImage);
+  //Copy image data
+};
+//////////////////////////////////////////////////////////////
+
+
   //Page 2 Enroll Button
   $("#enrollButton").on("click", function() {
     enrollUser(firstName, lastName, img);
@@ -222,8 +220,6 @@ var takepicture = function(video) {
     verifyUser(testFirst, testLast, verifyImage);
   });
   //Page 3 Webcam Verify Button
-  // $("#verifyButton").on("click", function() {
-  //   verifyUser(firstName,lastName,imageData);
   //   //test
   //   console.log('submit log' + imageData);
   //   console.log('submit log' + firstName);
@@ -269,3 +265,5 @@ var takepicture = function(video) {
     }
   );
 });
+
+  // console.log("TEST" +imageData);
