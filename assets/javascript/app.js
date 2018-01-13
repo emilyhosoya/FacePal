c7e395e6eb822139968307e2386ff34e2649ef0d
garviswbs <30781787+garviswbs@users.noreply.github.com>
2018-01-12 20:36:46 -0700

    find the JSON

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -177,7 +177,7 @@ $(document).ready(function() {
   });
 
   $("#compareButton").on("click", function() {
-    console.log(verifyUser());
+    console.log(findTheJSON);
   });
   //Page 3 Webcam Verify Button
   // $("#verifyButton").on("click", function() {
