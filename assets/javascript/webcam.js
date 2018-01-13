3d76e20388a350d7276a157ead52ed9c3efbb7b1
garviswbs <30781787+garviswbs@users.noreply.github.com>
2018-01-12 17:17:19 -0700

    remove onready from webcam

--- assets/javascript/webcam.js
+++ assets/javascript/webcam.js
@@ -1,79 +1,75 @@
-$(document).ready(function() {
-  
-  //let imageData = "";
-  let verifyImage = "";
+//let imageData = "";
+let verifyImage = "";
 
-  var webcamModule = function() {
-    var streaming = false;
-    var video = null;
+var webcamModule = function() {
+  var streaming = false;
+  var video = null;
 
-    // image return
+  // image return
 
-    // console.log("TEST" +imageData);
+  // console.log("TEST" +imageData);
 
-    (function() {
-      video = document.getElementById("webcamVideo");
-      navigator.mediaDevices
-        .getUserMedia({ audio: false, video: true })
-        .then(function(stream) {
-          if (navigator.mozGetUserMedia) {
-            video.mozSrcObject = stream;
-          } else {
-            var vendorURL = window.URL || window.webkitURL;
-            video.src = vendorURL.createObjectURL(stream);
+  (function() {
+    video = document.getElementById("webcamVideo");
+    navigator.mediaDevices
+      .getUserMedia({ audio: false, video: true })
+      .then(function(stream) {
+        if (navigator.mozGetUserMedia) {
+          video.mozSrcObject = stream;
+        } else {
+          var vendorURL = window.URL || window.webkitURL;
+          video.src = vendorURL.createObjectURL(stream);
+        }
+        video.play();
+        localStream = stream.getTracks()[0];
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+    video.addEventListener(
+      "canplay",
+      function(ev) {
+        if (!streaming) {
+          video.setAttribute("width", "600");
+          video.setAttribute("height", "450");
+          streaming = true;
+        }
+        var captureInterval = 5000;
+        var countdown = captureInterval / 1000;
+        var counterFunction = setInterval(function() {
+          $("#showCounter").html(countdown);
+
+          //Take the picture
+          if (countdown <= 0) {
+            takepicture(video);
+            clearInterval(counterFunction);
+            localStream.stop();
           }
-          video.play();
-          localStream = stream.getTracks()[0];
-        })
-        .catch(function(err) {
-          console.log(err);
-        });
-      video.addEventListener(
-        "canplay",
-        function(ev) {
-          if (!streaming) {
-            video.setAttribute("width", "600");
-            video.setAttribute("height", "450");
-            streaming = true;
-          }
-          var captureInterval = 5000;
-          var countdown = captureInterval / 1000;
-          var counterFunction = setInterval(function() {
-            $("#showCounter").html(countdown);
+          countdown--;
+        }, 1000);
+      },
+      false
+    );
+  })();
 
-            //Take the picture
-            if (countdown <= 0) {
-              takepicture(video);
-              clearInterval(counterFunction);
-              localStream.stop();
-            }
-            countdown--;
-          }, 1000);
-        },
-        false
-      );
-    })();
+  //console.log("verify image: " + verifyImage);
+};
 
-    //console.log("verify image: " + verifyImage);
-  };
+var takepicture = function(video) {
+  $("#showCounter").html("Retrieving data...");
+  var canvas = document.createElement("CANVAS");
+  var context = canvas.getContext("2d");
+  canvas.width = "600";
+  canvas.height = "450";
+  // draw video image onto canvas, get data
+  context.drawImage(video, 0, 0);
+  var imageData = canvas.toDataURL("image/png");
+  $("#showCounter").html("See image data in console.");
+  $(video).hide();
+  //console.log(imageData); ///return
 
-  var takepicture = function(video) {
-    $("#showCounter").html("Retrieving data...");
-    var canvas = document.createElement("CANVAS");
-    var context = canvas.getContext("2d");
-    canvas.width = "600";
-    canvas.height = "450";
-    // draw video image onto canvas, get data
-    context.drawImage(video, 0, 0);
-    var imageData = canvas.toDataURL("image/png");
-    $("#showCounter").html("See image data in console.");
-    $(video).hide();
-    //console.log(imageData); ///return
-
-    //return imageData;
-    verifyImage = imageData.split(",")[1];
-    console.log("Webcam.js testing : "+verifyImage)
-    //Copy image data
-  };
-
-});
+  //return imageData;
+  verifyImage = imageData.split(",")[1];
+  console.log("Webcam.js testing : "+verifyImage)
+  //Copy image data
+};
